fa0d4a630b42a8666f6f91f624cab84a274e0838
julianwills <80351642+julianwills@users.noreply.github.com>
2024-11-08 18:51:37 +0000

    Session 1

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,11 @@
 
 function Message () {
-    return <h1>Hello world</h1>;
+
+    const name = null;// 'Julian';
+
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello someone</h1>
 }
 
 export default Message;
